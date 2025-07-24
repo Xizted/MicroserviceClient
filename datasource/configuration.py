1cf3871ad2210748a7d8393a74ad503501eb5859
Osmar Ortiz <xizted79@gmail.com>
2025-07-24 17:44:27 -0500

    fix

--- datasource/configuration.py
+++ datasource/configuration.py
@@ -7,6 +7,8 @@ password_db = get_secret("db-password")
 ip_db = os.getenv("IP_DB", "localhost")
 port_db = os.getenv("PORT_DB", "3306")
 
+print(f"→ IP_DB={ip_db}, PORT_DB={port_db}, NAME_DB={name_db}")  # Línea clave para debug
+
 class Config:
-    SQLALCHEMY_DATABASE_URI=f"mysql+pymysql://{user_db}:{password_db}@{ip_db}:{port_db}/{name_db}"
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user_db}:{password_db}@{ip_db}:{port_db}/{name_db}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
