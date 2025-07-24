e6f7102b658e36e0dd9eeab91243650f0122eb37
Osmar Ortiz <xizted79@gmail.com>
2025-07-24 17:34:37 -0500

    fix

--- datasource/configuration.py
+++ datasource/configuration.py
@@ -1,11 +1,11 @@
 import os
 from .secrets import get_secret
 
-name_db=os.getenv("appdb")
-user_db=get_secret("db-user")
-password_db=get_secret("db-password")
-ip_db=os.getenv("localhost")
-port_db=os.getenv("3306")
+name_db = os.getenv("NAME_DB", "appdb")
+user_db = get_secret("db-user")
+password_db = get_secret("db-password")
+ip_db = os.getenv("IP_DB", "localhost")
+port_db = os.getenv("PORT_DB", "3306")
 
 class Config:
     SQLALCHEMY_DATABASE_URI=f"mysql+pymysql://{user_db}:{password_db}@{ip_db}:{port_db}/{name_db}"
